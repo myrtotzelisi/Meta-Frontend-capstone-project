7b7b021c95ab9fef1a7e74daeb104ced94670724
myrtotzelisi <151649970+myrtotzelisi@users.noreply.github.com>
2024-05-13 19:51:49 +0200

    create a separate Components folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 
 import './App.css';
-import Header from './Header';
-import Nav from './Nav';
-import Main from './Main';
-import Footer from './Footer';
+import Header from './Components/Header';
+import Nav from './Components/Nav';
+import Main from './Components/Main';
+import Footer from './Components/Footer';
 
 function App() {
   return (
